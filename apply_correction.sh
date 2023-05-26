0e4340d8394be3fa917a1b0b59922349421d7628
Dave Barry <davejpbarry@gmail.com>
2023-05-26 13:03:49 +0100

    Update apply_correction.sh

--- apply_correction.sh
+++ apply_correction.sh
@@ -9,22 +9,12 @@
 
 files=(/nemo/stp/lm/working/barryd/Working_Data/Santos/Elias/Compiled_Stacks/*.tif)
 
-#IFS=".tif"
-
-#cd /nemo/stp/lm/working/barryd/Working_Data/Santos/Elias/Compiled_Stacks
-
-#files=($(ls --ignore="*DAPI*"))
-
 IFS="--"
 
 read -a strarr <<< "${files[$SLURM_ARRAY_TASK_ID]}"
 
 dapiPath="${strarr[0]}--${strarr[2]}--${strarr[4]}--${strarr[6]}--${strarr[8]}--DAPI_2023-May-25-001"
 dapiFolder=$(basename "${dapiPath}")
-
-echo "$dapiPath"
-
-echo "$dapiFolder"
 
 IFS="_"
 
@@ -33,8 +23,6 @@ read -a strarr2 <<< "$dapiFolder"
 IFS=" "
 
 settingsFile="$dapiPath/${strarr2[0]}_${strarr2[1]}_settings.csv"
-
-echo $settingsFile
 
 ml Java/1.8
 /nemo/stp/lm/working/barryd/hpc/java/fiji/ImageJ-linux64 -Xmx4G -- --ij2 --headless --console --run /nemo/stp/lm/working/barryd/hpc/java/fiji/plugins/Fast4DReg/channel_apply.ijm 'files='\""${files[$SLURM_ARRAY_TASK_ID]}"\"',settings_file_path='\""$settingsFile"\"',results_path="/nemo/stp/lm/working/barryd/Working_Data/Santos/Elias/Aligned_Stacks"'
