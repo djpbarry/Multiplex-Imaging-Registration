1f65809965fe5915574c2ebedc582676ddf0462e
Dave Barry <davejpbarry@gmail.com>
2024-02-03 16:26:19 +0000

    Update fiji_stack_building.sh

--- fiji_stack_building.sh
+++ fiji_stack_building.sh
@@ -3,11 +3,18 @@
 #SBATCH --job-name=fiji-conversion
 #SBATCH --ntasks=1
 #SBATCH --time=1:00:00
-#SBATCH --array=0-4608
+#SBATCH --array=0-9
 #SBATCH --partition=cpu
 #SBATCH --mem=4G
 
-files=(/nemo/stp/lm/inputs/santoss/Elias\ Copin/Multiplexed_imaging_data/OM_96w_8seq_20210706_4i_16th_stain_Bry_488_pS10H3_563_MASTL_647_001/data/*.tif)
+# The parent directory should point to the top level directory where all your images are contained
+parentDir=(/nemo/stp/lm/inputs/santoss/Elias\ Copin/Multiplexed_imaging_data)
+# The output directory is where the compiled stacks will appear
+outputDir=(/nemo/stp/lm/working/barryd/Working_Data/Santos/Elias/Compiled Stacks)
+# The FIJI directory should, as the name suggests, point to the location of your FIJI installation
+fijiDir=(/nemo/stp/lm/working/barryd/hpc/java/fiji)
+
+files=("$parentDir"/*/*/*.tif)
 
 ml Java/1.8
-/nemo/stp/lm/working/barryd/hpc/java/fiji/ImageJ-linux64 -Xmx4G -- --headless --console -macro "./build_stacks.ijm" "/nemo/stp/lm/inputs/santoss/Elias Copin/Multiplexed_imaging_data,/nemo/stp/lm/working/barryd/Working_Data/Santos/Elias/Compiled Stacks,${files[$SLURM_ARRAY_TASK_ID]}"
+"$fijiDir"/ImageJ-linux64 -Xmx4G -- --headless --console -macro "./build_stacks.ijm" "$parentDir,$outputDir,${files[$SLURM_ARRAY_TASK_ID]}"
