2317cdb489205a113121186db47d2c933020bead
Dave Barry <davejpbarry@gmail.com>
2024-02-03 17:40:22 +0000

    Update apply_correction.sh

--- apply_correction.sh
+++ apply_correction.sh
@@ -3,12 +3,12 @@
 #SBATCH --job-name=drift_correction
 #SBATCH --ntasks=1
 #SBATCH --time=1:00:00
-#SBATCH --array=0-3457
+#SBATCH --array=0-9
 #SBATCH --partition=cpu
 #SBATCH --mem=4G
 
 # The input directory should contain a series of compiled TIF stacks (the output from Step 1)
-inputDir=(/nemo/stp/lm/working/barryd/Working_Data/Santos/Elias/Compiled Stacks)
+inputDir=(/nemo/stp/lm/working/barryd/hpc/test/stacks)
 # The FIJI directory should, as the name suggests, point to the location of your FIJI installation
 fijiDir=(/nemo/stp/lm/working/barryd/hpc/java/fiji)
 # The directory in which you want your fully aligned stacks to be saved
@@ -20,7 +20,7 @@ IFS="--"
 
 read -a strarr <<< "${files[$SLURM_ARRAY_TASK_ID]}"
 
-dapiPath=$(find "$inputStacksDir" -path "*${strarr[2]}--${strarr[4]}--${strarr[6]}--${strarr[8]}*" -type d)
+dapiPath=$(find "$inputDir" -path "*${strarr[2]}--${strarr[4]}--${strarr[6]}--${strarr[8]}*" -type d)
 dapiFolder=$(basename "${dapiPath}")
 
 IFS="_"
@@ -32,4 +32,4 @@ IFS=" "
 settingsFile="$dapiPath/${strarr2[0]}_${strarr2[1]}_settings.csv"
 
 ml Java/1.8
-"$fijiDir"/ImageJ-linux64 -Xmx4G -- --ij2 --headless --console --run ./channel_apply.ijm 'files='\""${files[$SLURM_ARRAY_TASK_ID]}"\"',settings_file_path='\""$settingsFile"\"',results_path='\""$outputDir"\"'
+"$fijiDir"/ImageJ-linux64 -Xmx4G -- --ij2 --headless --console --run ./channel_apply.ijm 'files='\""${files[$SLURM_ARRAY_TASK_ID]}"\"',settings_file_path='\""$settingsFile"\"',results_path='\""$outputDir"\"
