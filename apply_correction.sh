bd0b23be6fabdb148610347b0aa51871864730a5
Dave Barry <davejpbarry@gmail.com>
2024-02-03 16:33:43 +0000

    Update apply_correction.sh

--- apply_correction.sh
+++ apply_correction.sh
@@ -7,8 +7,14 @@
 #SBATCH --partition=cpu
 #SBATCH --mem=4G
 
-inputStacksDir="/nemo/stp/lm/working/barryd/hpc/test/test_outputs"
-files=("$inputStacksDir"/*.tif)
+# The input directory should contain a series of compiled TIF stacks (the output from Step 1)
+inputDir=(/nemo/stp/lm/working/barryd/Working_Data/Santos/Elias/Compiled Stacks)
+# The FIJI directory should, as the name suggests, point to the location of your FIJI installation
+fijiDir=(/nemo/stp/lm/working/barryd/hpc/java/fiji)
+# The directory in which you want your fully aligned stacks to be saved
+outputDir=(/nemo/stp/lm/working/barryd/hpc/test/aligned_stacks)
+
+files=("$inputDir"/*.tif)
 
 IFS="--"
 
@@ -26,4 +32,4 @@ IFS=" "
 settingsFile="$dapiPath/${strarr2[0]}_${strarr2[1]}_settings.csv"
 
 ml Java/1.8
-/nemo/stp/lm/working/barryd/hpc/java/fiji/ImageJ-linux64 -Xmx4G -- --ij2 --headless --console --run ./channel_apply.ijm 'files='\""${files[$SLURM_ARRAY_TASK_ID]}"\"',settings_file_path='\""$settingsFile"\"',results_path="/nemo/stp/lm/working/barryd/hpc/test/aligned_stacks"'
+"$fijiDir"/ImageJ-linux64 -Xmx4G -- --ij2 --headless --console --run ./channel_apply.ijm 'files='\""${files[$SLURM_ARRAY_TASK_ID]}"\"',settings_file_path='\""$settingsFile"\"',results_path='\""$outputDir"\"'
