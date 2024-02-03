511f2190df425c800f7f3f314045cba4c8b310f9
Dave Barry <davejpbarry@gmail.com>
2024-02-03 17:24:00 +0000

    Update fiji_stack_building.sh

--- fiji_stack_building.sh
+++ fiji_stack_building.sh
@@ -10,11 +10,13 @@
 # The parent directory should point to the top level directory where all your images are contained
 parentDir=(/nemo/stp/lm/inputs/santoss/Elias\ Copin/Multiplexed_imaging_data)
 # The output directory is where the compiled stacks will appear
-outputDir=(/nemo/stp/lm/working/barryd/Working_Data/Santos/Elias/Compiled Stacks)
+outputDir=(/nemo/stp/lm/working/barryd/hpc/test/stacks)
 # The FIJI directory should, as the name suggests, point to the location of your FIJI installation
 fijiDir=(/nemo/stp/lm/working/barryd/hpc/java/fiji)
 
-files=("$parentDir"/*/*/*.tif)
+childDirs=("$parentDir"/*)
+
+files=("${childDirs[15]}"/data/*.tif)
 
 ml Java/1.8
 "$fijiDir"/ImageJ-linux64 -Xmx4G -- --headless --console -macro "./build_stacks.ijm" "$parentDir,$outputDir,${files[$SLURM_ARRAY_TASK_ID]}"
